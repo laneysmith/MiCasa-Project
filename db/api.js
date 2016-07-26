c84c8e16009cd7aeb7d8e327576f8068fe80e070
Lucas Barbula <lucas.barbula@gmail.com>
2016-07-26 17:21:05 -0600

    file restructure

--- db/api.js
+++
@@ -1,7 +0,0 @@
-var knex = require('./knex');
-
-module.exports = {
-  findAllBusinesses: function (type) {
-    return knex('business').select();
-  }
-};
--- seeds/04_industry.js
+++
@@ -1,6 +0,0 @@
-exports.seed = function(knex, Promise) {
-	return Promise.all([
-		knex('industry').insert({name: 'Agriculture'}),
-		knex('industry').insert({name: 'Construction'})
-	]);
-};
