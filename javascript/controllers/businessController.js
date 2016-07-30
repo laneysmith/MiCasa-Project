c915f1494360d24981755fa68805e803407153aa
Will Thomas <wlthomas001@gmail.com>
2016-07-30 12:25:20 -0600

    pull in business-owner data from server and display in business lookup, add search capability

--- javascript/controllers/businessController.js
+++ javascript/controllers/businessController.js
@@ -2,6 +2,7 @@ angular
 .module('myApp')
 .controller('businessController', function($scope, $http) {
   $http.get('http://localhost:3000/businesses').then(function(data) {
-    console.log(data);
+    $scope.resultArray = data.data
+    console.log($scope.resultArray);
   })
 })
