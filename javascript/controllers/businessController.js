f10cb9fc7c456c37b538d507b157ba675aca9501
Laney Smith <lsmith.unc@gmail.com>
2016-08-07 12:58:44 -0600

    Working industry dropdown

--- javascript/controllers/businessController.js
+++ javascript/controllers/businessController.js
@@ -1,8 +1,13 @@
 angular
-.module('myApp')
-.controller('businessController', function($scope, dataFactory) {
-   dataFactory.getAll
-    .then(function(data){
-        $scope.resultArray = data
-    })
-})
+	.module('myApp')
+	.controller('businessController', function($scope, dataFactory) {
+		dataFactory.getAll
+			.then(function(data) {
+				$scope.resultArray = data
+			})
+		dataFactory.getAllIndustries
+			.then(function(data) {
+				console.log(data);
+				$scope.industries = data
+			})
+	})
--- javascript/factories.js
+++ javascript/factories.js
@@ -1,19 +1,24 @@
 angular
-  .module('myApp')
-  .factory('dataFactory', ['$http', '$q', function($http, $q){
-    return {
-      getAll:
-        $http.get('http://localhost:3000/businesses')
-        .then(function(data) {
-          let results = data.data
-          return $q.resolve(results)
-        }),
-      getById: function(id) {
-        return $http.get('http://localhost:3000/businesses/' + id)
-        .then(function(data) {
-          let business = data.data
-            return $q.resolve(business)
-        })
-      }
-    }
-  }])
+	.module('myApp')
+	.factory('dataFactory', ['$http', '$q', function($http, $q) {
+		return {
+			getAll: $http.get('http://localhost:3000/businesses')
+				.then(function(data) {
+					let results = data.data
+					return $q.resolve(results)
+				}),
+			getById: function(id) {
+				return $http.get('http://localhost:3000/businesses/' + id)
+					.then(function(data) {
+						let business = data.data
+						return $q.resolve(business)
+					})
+			},
+			getAllIndustries: $http.get('http://localhost:3000/businesses/industries')
+				.then(function(data) {
+					console.log(data);
+					let results = data.data
+					return $q.resolve(results)
+				})
+		}
+	}])
