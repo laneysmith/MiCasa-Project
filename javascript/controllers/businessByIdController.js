4b807869ac7acada733b77009963eedd1112949f
Laney Smith <lsmith.unc@gmail.com>
2016-08-09 14:29:22 -0600

    Update business edit modal

--- javascript/controllers/businessByIdController.js
+++ javascript/controllers/businessByIdController.js
@@ -1,12 +1,16 @@
 angular
-.module('myApp')
-.controller('businessByIdController', function($scope,  $stateParams, dataFactory) {
-  $scope.business = {}
-  $scope.commentArray = []
-  dataFactory.getById($stateParams.id)
-    .then(function(data){
-      $scope.commentArray = data[0]
-      console.log(data)
-      $scope.business = data[1][0]
-    })
-})
+	.module('myApp')
+	.controller('businessByIdController', function($scope, $stateParams, dataFactory) {
+		$scope.business = {}
+		$scope.commentArray = []
+		dataFactory.getById($stateParams.id)
+			.then(function(data) {
+				$scope.commentArray = data[0]
+				console.log(data)
+				$scope.business = data[1][0]
+			})
+		dataFactory.getAllIndustries
+			.then(function(data) {
+				$scope.industries = data
+			})
+	})
