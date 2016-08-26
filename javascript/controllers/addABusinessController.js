e4e3c4840b1d5eca14dd685553294b22da8e4369
Will Thomas <wlthomas001@gmail.com>
2016-08-26 17:00:43 -0600

    work on add business, education specifically

--- javascript/controllers/addABusinessController.js
+++ javascript/controllers/addABusinessController.js
@@ -1,10 +1,22 @@
 angular
 	.module('myApp')
-  .controller('addABusinessController', function($scope, dataFactory) {
+  .controller('addABusinessController', function($scope, dataFactory, $state) {
 	dataFactory.getAllIndustries.then(function(data) {
 		console.log(data);
 		$scope.industries = data;
 	})
+	dataFactory.getAllClasses.then(function(data) {
+		console.log(data);
+		$scope.classes = data;
+	})
+
+	$scope.addBusiness = function (business, owner) {
+		console.log("Client Business",business);
+		console.log("Owner with Education?", owner);
+			dataFactory.addBusiness(business, owner).then(function() {
+				$state.go('home')
+			})
+	}
 
 
 
--- javascript/factories.js
+++ javascript/factories.js
@@ -27,7 +27,7 @@ angular
 					'zip': body.zip,
 					'email': body.email,
 					'phone': body.phone,
-					'industry_id': body.industry_id,
+					'industry_id': parseInt(body.industry_id),
 					'date_opened': body.date_opened,
 					'date_closed': body.date_closed,
 					'good_standing': body.good_standing,
@@ -36,10 +36,48 @@ angular
 				}
 				return $http.put('http://localhost:3000/businesses/' + body.business_id, newData)
 			},
+			addBusiness: function (business, owner) {
+				businessData = {
+					'name': business.name,
+					'address1': business.address1,
+					'city': business.city,
+					'state': business.state,
+					'zip': business.zip,
+					'email': business.email,
+					'phone': business.phone,
+					'industry_id': business.industry_id,
+					'date_opened': business.date_opened,
+					'date_closed': business.date_closed,
+					'good_standing': business.good_standing,
+					'description': business.description,
+					'website': business.website
+				}
+
+				ownerData = {
+					'first_name': owner.first_name,
+					'last_name': owner.last_name,
+					'email': owner.email,
+					'phone': owner.phone,
+					'did_graduate': owner.did_graduate,
+					'year': owner.year,
+					'semester': owner.semester,
+					'class_id': owner.class_id
+				}
+
+				var data = [businessData,ownerData]
+
+				return $http.post('http://localhost:3000/businesses/add', data)
+			},
+
 			getAllIndustries: $http.get('http://localhost:3000/businesses/industries')
 				.then(function(data) {
 					let results = data.data
 					return $q.resolve(results)
+				}),
+			getAllClasses: $http.get('http://localhost:3000/businesses/classes')
+					.then(function(data) {
+						let results = data.data
+						return $q.resolve(results)
 				}),
 			getAllCities: $http.get('http://localhost:3000/businesses/cities')
 				.then(function(data) {
